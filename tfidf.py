b3bca364838d7a29255e212f96d4ccbd86bc3914
Brahma S. Pavse <brahmasp@utexas.edu>
2017-04-21 00:18:27 -0500

    Impling tfidf

---
+++ tfidf.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+
+'''
+Code below performs TDIDF numerical stats on a given mystery file to identify
+and other data sets (that "train" the algorithm 
+
+TFIDF: Measures the importance of a word in a given corpus
+'''
+from collections import Counter
+import glob
+import math
+import re
+import operator
+
+# Method to remove top and bottom metadata info from corpus
+def remove_book_metadata(text):
+	re_start = re.search(r'\*\*\*\s?START.*\*\*\*', text)
+	re_end = re.search(r'\*\*\*\s?END.*\*\*\*', text)
+	core_text = text[re_start.span()[1] : re_end.span()[0]-1].strip()
+
+	return core_text;
+
+# TODO
+# TEMP: hardcoded to include alphanumeric and apostrophe
+def filter_words(words, filter):
+
+	for index, word in enumerate(words):
+		words[index] = re.sub(r"[^\w']+", "", word, flags=re.UNICODE);
+
+# Method to get word to frequency map
+def get_word_freq(words):
+	return dict(Counter(words));
+
+
+def get_tfidf_map(file_list_contents, word_counts_per_file):
+
+	tfidf_map = {};
+
+	total_docs = len(file_list_contents);
+
+	# For each file
+	for filename in file_list_contents:
+
+		# Get word count freq for each file
+		word_count_freq = word_counts_per_file[filename]
+		tfidf_scores = {};
+
+		# Get count of the given word and check occurence
+		# in other docs
+		for word in word_count_freq:
+			count_per_doc = word_count_freq[word];
+			occurence_other_doc = 0;
+
+			# Go through all the word count maps
+			# of each file to see if the current word
+			# exists in other docs.
+			# Value should be minimum always 1
+			for w in word_counts_per_file:
+				if word in word_counts_per_file[w]:
+					occurence_other_doc += 1;
+
+			tfidf_score = count_per_doc * math.log10(total_docs / occurence_other_doc);
+			tfidf_scores[word] = tfidf_score;
+		
+		tfidf_map[filename] = tfidf_scores;
+
+def train_data_word_freq():
+
+
+	file_list_contents = [];
+
+    # Dictionary of filename to word_freq map
+	word_counts_per_file = {};
+
+    # Cycle through all the test files
+	for filename in glob.glob('books/*.txt'):
+		f = open(filename, 'r', encoding='utf8')
+
+		# Read the file
+		text = f.read()
+
+		# Extracting core content - removing metadata
+		text = remove_book_metadata(text);
+
+		# Vector of all the from a given book
+		words = text.split();
+
+		# Filter words to include alphanumeric and apostrophe
+		filter_words(words, "");
+
+
+		# Need to get word-frequency of this text and store it
+		# This information is linked to the author who wrote it
+
+		# Store the information and just compare it
+		word_freq = get_word_freq(words);
+
+		if filename not in word_counts_per_file:
+		    word_counts_per_file[filename] = word_freq;
+		else:
+		    print("Error: same file name! " + filename)
+
+		if filename not in file_list_contents:
+			file_list_contents.append(filename);
+		else:
+			print("Content of file already recorded");
+
+	
+	# At this point
+	# Have map from file name to its contents
+	# Have map from file name to count of each word in that file
+
+	"""
+	iterate through map keys
+	
+	outer for loop is iterating over files
+	inner for loop iterating over all words in the doc
+		for a given word in a given doc
+		can get the count
+		but need to iterate over other maps and see if word
+		exists
+
+		then get tfidf for that word and given doc and store it
+	"""
+
+	tfidf_map = get_tfidf_map(file_list_contents, word_counts_per_file);
+
+	return tfidf_map;
+
+def detect_author_word_freq(data_stats, test_name):
+
+	file_list_contents = [];
+
+    # Dictionary of filename to word_freq map
+	word_counts_per_file = {};
+
+	for test_filename in glob.glob(test_name):    
+		f = open(test_filename, 'r', encoding='utf8')
+
+		# Read the file
+		test_text = f.read()
+
+		# Extracting core content - removing metadata
+		test_text = remove_book_metadata(test_text);
+
+		# Vector of all the from a given book
+		test_words = test_text.split();
+
+		# Filter words to include alphanumeric and apostrophe
+		filter_words(test_words, "");
+
+		# Need to get word-frequency of this text and store it
+		# This information is linked to the author who wrote it
+
+		# Store the information and just compare it
+		test_word_freq = get_word_freq(test_words);
+
+
+		# Need to check similarity of this test item to trained
+		# corupus.
+		# Traverse through test data and compare frequency for
+		# a given word with training data
+		# Count the number of times there was a success
+		# Success is measured if given the word existed in both dictionaries
+		# its difference in count == 0
+
+
+		if test_filename not in word_counts_per_file:
+		    word_counts_per_file[test_filename] = test_word_freq;
+		else:
+		    print("Error: same file name! " + test_filename)
+
+		if test_filename not in file_list_contents:
+			file_list_contents.append(test_filename);
+		else:
+			print("Content of file already recorded");
+
+
+def main():
+	
+	# Get tfidf for all words
+	# sort list of tfidf stats and their corresponding word
+
+	# a tfidf belongs to a document (not author)
+	# 2 stores by same author have two different tfids
+
+	# compute tfidf for the mystery document
+	# compare its tfidf with all of the tfidfs avilable and check
+	# similarity
+
+	# Another way, compute average of tfidfs from different docs
+	# by a COMMON author and then compare it with the tfidf
+	# of the new doc
+
+
+	# After training the data structure should be
+	# Map within map
+	# {file_name: {word1: tfidf, word2: tfdif score}}
+
+	"""
+	tfidf 
+	= count(word, in one doc) 
+		* log(total docs / num docs it appears in)
+	"""
+
+	tfidf_map = train_data_word_freq();
+
+	# Testing all files in the test_data directory
+	for test_filename in glob.glob('test_data/*.txt'):
+		print(" ");
+		print("Currently testing file: " + test_filename); 
+		detect_author_word_freq(tfidf_map, test_filename);
+
+
+
+
+main();
+
