623624bd6f6327ef4e36e859ead39498163367fb
Aditya Vaidya <kroq.gar78@gmail.com>
2017-04-25 18:21:31 -0500

    Write file to just generate POS tags and store in file

---
+++ gen_pos.py
@@ -0,0 +1,40 @@
+import nltk
+from collections import Counter
+import glob
+import math
+import re
+import operator
+import json
+from nltk.corpus import brown
+from nltk.tokenize import RegexpTokenizer
+from word_freq_impl import remove_book_metadata, dict_mag, cosine_similarity, cosine_based_closeness
+
+def gen_pos(filename):
+    f = open(filename, 'r', encoding='utf8')
+
+    # Read the file
+    text = f.read()
+
+    # Extracting core content - removing metadata
+    text = remove_book_metadata(text);
+
+    # tokenize the file and split it into its POS tuples
+    tokenizer = RegexpTokenizer(r'\w+')
+    text = tokenizer.tokenize(text)
+
+    pos_tuples = nltk.pos_tag(text)
+
+    return pos_tuples
+
+def write_pos_csv(tuples, outfile):
+    print("Writing to file",outfile)
+    with open(outfile, 'w') as f:
+        f.write(json.dumps(tuples))
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Need file to generate with", sys.stderr)
+
+    # print(sys.getsizeof(gen_pos(sys.argv[1])))
+    write_pos_csv(gen_pos(sys.argv[1]), re.sub(r'\.txt$',"_pos.json",sys.argv[1]))
